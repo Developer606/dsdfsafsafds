28fe17ee630db5af09faca862831673e5d49fa82
lefagej483 <39846669-lefagej483@users.noreply.replit.com>
2025-02-20 17:03:41 +0000

    Agent query: Can you try creating a new character and then clicking on it to start a chat? Is it working now?
    Make anime characters clickable and chattable.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/16b4ba8b-6ee2-4b1c-a64d-7d288de52d2c/29a9991c-e2c6-4188-a65e-e9217b775792.jpg

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -29,9 +29,7 @@ export default function Home() {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
           {characters?.map((character) => (
             <Link key={character.id} href={`/chat/${character.id}`}>
-              <a className="transform transition-transform hover:scale-105">
-                <CharacterCard character={character} />
-              </a>
+              <CharacterCard character={character} />
             </Link>
           ))}
         </div>
